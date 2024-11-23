1a4010ecda229d277ed19ec536cef4244ab71593
alex_barmen <www.lad282@gmail.com>
2024-11-23 16:30:35 +0300

    feat: DataFromSite registered in admin panel

--- project_root/app/admin.py
+++ project_root/app/admin.py
@@ -1,10 +1,21 @@
 from django.contrib import admin
 
-from app.models import UserData
+from app.models import UserData, DataFromSite
 
 
 @admin.register(UserData)
 class UserDataAdmin(admin.ModelAdmin):
     list_display = ['name', 'business_type', 'region', 'industry', 'is_innovative']
     list_display_links = ['name']
+    
+
+@admin.register(DataFromSite)
+class DataFromSite(admin.ModelAdmin):
+    list_display = [
+        'id',
+        'site_url',
+        ]
+    list_display_links = [
+        'id'
+    ]
     
